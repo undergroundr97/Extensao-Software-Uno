ce31bb6826a293480c3e2ac25d9a937f3c3a2bc5
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2025-04-30 17:08:34 -0300

    .

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const map = L.map('map').setView([0, 0], 13);
-import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+import { collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors',
